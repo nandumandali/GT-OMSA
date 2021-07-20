16a57bbcc9aa12256c9b320d325e28e7703b3fe3
Mandali, Yoganand <nandumandali@gmail.com>
2021-07-19 21:58:57 -0500

    Add files via Upload

---
+++ Week 9/HW9-Q2.R
@@ -0,0 +1,3 @@
+library(FrF2)
+result <- FrF2(nruns=16, nfactors=10, factor.names=c("Location","Age","N_Beds","Yard","Upgrades","Condition","Area","Neighborhood","School District","Garage"))
+result
